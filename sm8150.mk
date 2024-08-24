1fe6c43bbc296b92e74e9d260203d0203d64e48e
hridaya <hridaya@pixelos.net>
2024-08-24 14:53:06 +0700

    sm8150-common: Drop mdm related entries
    Output of 'getprop ro.baseband' gives 'msm', which results in not starting vendor.mdm_helper anyways.
    
    Change-Id: I663be2c5628c9e317ad9cd5b3ff84fd17689ae7f

--- sm8150.mk
+++ sm8150.mk
@@ -427,7 +427,6 @@ PRODUCT_PACKAGES += \
     init.lge.usb.default.sh \
     init.lge.usb.lao.sh \
     init.lge.usb.sh \
-    init.mdm.sh \
     init.qcom.devstart.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
