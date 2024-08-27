c691107cba49d2749796b13134414f21546b4838
Tuan Anh <tuan73176@gmail.com>
2024-08-27 14:03:12 +0700

    sm8150-common: Add compatibility matrix xml from LineageOS
    StatiX does not have it in hardware/qcom-caf/common. Kang it from LineageOS.
    Snapshot of LineageOS/android_hardware_qcom-caf_common@d0d2fa3f047a78924997417c80dcf42fab2def8d.
    
    Change-Id: I1abd251ac370371019a5faf4d77f8b90a0898e64

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,7 +101,7 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
